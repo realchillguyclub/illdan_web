1ce811fd77a08e2985f969d4dd170b772382bff8
dogmania <hj296914@gmail.com>
2025-09-10 16:12:26 +0900

    🍱 Chore: 리소스 추가

---
+++ composeApp/src/wasmJsMain/kotlin/com/chillguy/illdanweb/core/design_system/Colors.kt
@@ -0,0 +1,12 @@
+package com.chillguy.illdanweb.core.design_system
+
+import androidx.compose.ui.graphics.Color
+
+object AppColor {
+    val BACKGROUND1 = Color(0xFFC0FBFF)
+    val BACKGROUND2 = Color(0xFF9DDFFF)
+    val BACKGROUND3 = Color(0xFF84CBFF)
+    val BACKGROUND4 = Color(0xFFBBF7FF)
+
+    val BLACK = Color(0xFF000000)
+}
---
+++ composeApp/src/wasmJsMain/kotlin/com/chillguy/illdanweb/core/design_system/String.kt
@@ -0,0 +1,2 @@
+package com.chillguy.illdanweb.core.design_system
+
