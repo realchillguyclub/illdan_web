1e7aca4455ae65dfcde802b43ea131d8a4e95aac
dogmania <hj296914@gmail.com>
2025-09-11 14:30:43 +0900

    ✨ Feat: Scale 도메인 모델 정의

---
+++ composeApp/src/wasmJsMain/kotlin/com/chillguy/illdanweb/domain/model/Scale.kt
@@ -0,0 +1,15 @@
+package com.chillguy.illdanweb.domain.model
+
+import androidx.compose.ui.unit.Dp
+
+data class Scale(
+    val gutter: Dp,
+    val contentMaxWidth: Dp,
+    val topSpace: Dp,
+    val spaceAfterDownload: Dp,
+    val spaceAfterIntro: Dp,
+    val bottomSpace: Dp,
+    val appIconSize: Dp,
+    val storeBtnHeight: Dp,
+    val storeBtnGap: Dp
+)
